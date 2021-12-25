8bd756f0bb8f4558a3545177111c18020ef54840
Sindre SVENDSRUD <sindre.svendsrud@soprasteria.com>
2021-12-25 15:01:55 +0100

    changed ps

--- CubeItUp.Infrastructure/DeployInfrastructure.ps1
+++ CubeItUp.Infrastructure/DeployInfrastructure.ps1
@@ -1,17 +1,15 @@
 param (
     [string]$environment
 )
-$environmentPrefix = 'd'
-if ($environment == 'production') {
-    $environmentPrefix = 'p'
+$environmentPrefix = "d"
+if ($environment == "production") {
+    $environmentPrefix = "p"
 }
 
-$ResourceGroupName = $environmentPrefixCubeItUpRg
+$ResourceGroupName = $environmentPrefix + "CubeItUpRg"
 
 #Create resource group
 New-AzResourceGroup -Name $ResourceGroupName -Location "West Europe"
 
 #Deploy app service plan
 New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile 'ArmTemplates/AppServicePlan/Template.json'
-
-C:\Repos\cubeitup\CubeItUp.Infrastructure\DeployInfrastructure.ps1
