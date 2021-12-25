a0431114d45d0526f17a9488c1691875df9e7e07
Sindre SVENDSRUD <sindre.svendsrud@soprasteria.com>
2021-12-25 14:00:23 +0100

    added infrastructure script

---
+++ CubeItUp.Infrastructure/DeployInfrastructure.ps1
@@ -0,0 +1,15 @@
+param (
+    [string]$environment
+)
+$environmentPrefix = 'd'
+if ($environment == 'production') {
+    $environmentPrefix = 'p'
+}
+
+$ResourceGroupName = $environmentPrefix'CubeItUpRg'
+
+#Create resource group
+New-AzResourceGroup -Name $ResourceGroupName -Location "West Europe"
+
+#Deploy app service plan
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile 'ArmTemplates/AppServicePlan/Template.json'
