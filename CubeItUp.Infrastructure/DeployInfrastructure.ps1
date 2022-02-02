0b3ddcc8620ee28e13edb6ef14b878cfba0797ee
Sindresvends <sindre.svendsrud@gmail.com>
2022-02-02 20:56:30 +0100

    Update DeployInfrastructure.ps1

--- CubeItUp.Infrastructure/DeployInfrastructure.ps1
+++ CubeItUp.Infrastructure/DeployInfrastructure.ps1
@@ -8,18 +8,9 @@ if ($environment -eq "production") {
 Install-Module -Name Az -AllowClobber -force
 Install-Module -Name Az.Resources -AllowClobber -force
 $ResourceGroupName = $environmentPrefix + "CubeItUpRg"
-Write-Host $ResourceGroupName
-Write-Host $environment
-Write-Host $environmentPrefix
-#dir
-#cd $(Pipeline.Workspace)
-#dir
-#cd CubeItUpInfrastructure
-#dir
+
 #Create resource group
-#New-AzResourceGroup -Name $ResourceGroupName -Location "West Europe"
 az group create -l westeurope -n $ResourceGroupName
 
 #Deploy app service plan
-#New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile '../CubeItUpInfrastructure/ArmTemplates/AppServicePlan/Template.json' -TemplateParameterFile '../CubeItUpInfrastructure/ArmTemplates/AppServicePlan/Parameters.json'
 az deployment group create --resource-group $ResourceGroupName --template-file ../CubeItUpInfrastructure/ArmTemplates/AppServicePlan/Template.json
