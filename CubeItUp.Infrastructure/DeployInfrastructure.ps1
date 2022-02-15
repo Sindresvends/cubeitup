38372969e707e7b71f29841645057f63209b31e2
Sindresvends <sindre.svendsrud@gmail.com>
2022-02-15 21:35:53 +0100

    Update DeployInfrastructure.ps1

--- CubeItUp.Infrastructure/DeployInfrastructure.ps1
+++ CubeItUp.Infrastructure/DeployInfrastructure.ps1
@@ -6,29 +6,18 @@ $environmentPrefix = "d"
 if ($environment -eq "prod") {
     $environmentPrefix = "p"
 }
-#Install-Module -Name Az -AllowClobber -force
-#Install-Module -Name Az.Resources -AllowClobber -force
 
 ######naming resources, all resources should use resourcePrefix######
 $resourcePrefix = $environmentPrefix + "ciu"
 $resourceGroupName = $resourcePrefix + "rg"
 
-#Dir
-#cd ..
-#dir
-#cd CubeItUpInfrastructure
-#dir
-#cd ArmTemplates
-#dir
-#cd CosmosDb
-#dir
-
-#cosmos db
+################Cosmos Db############################################
 $cosmosDbAccountName = $resourcePrefix + "account"
 $cosmosDbName = $resourcePrefix + "db"
 $cosmosDbContainer = $resourcePrefix + "container"
 
-######################################################################
+##############Container app##########################################
+$containerAppEnvironmentName = $resourcePrefix + "en"
 
 #Create resource group
 New-AzResourceGroup -Name $resourceGroupName -Location "west europe" -Force
@@ -40,7 +29,6 @@ New-AzResourceGroup -Name $resourceGroupName -Location "west europe" -Force
 
 #Deploy cosmos db
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "../CubeItUpInfrastructure/ArmTemplates/CosmosDb/template.json" -Force
-#-accountName $cosmosDbAccountName
-#az deployment group create --resource-group $ResourceGroupName --template-file ../CubeItUpInfrastructure/ArmTemplates/CosmosDb/template.json 
-#--parameters "{ \"location\": { \"value\": \"westus\" } }"
-
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+    -TemplateFile "../CubeItUpInfrastructure/ArmTemplates/ContainerApp/template.json" -Force `
+    -environmentName $containerAppEnvironmentName
